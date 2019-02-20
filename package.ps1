ac668eb88ecc6a8a2b0426d64bda2ac29d9ad6f2
Kied T L <eli.georgios@gmail.com>
2019-02-20 09:05:13 -0500

    Update package.ps1

--- package.ps1
+++ package.ps1
@@ -21,4 +21,4 @@ dotnet publish -c Release --self-contained true -r osx.10.13-x64
 7z a dist\sultan-rhel64.zip bin\Release\netcoreapp2.1\rhel.7.4-x64\publish\*
 7z a dist\sultan-ubun64.zip bin\Release\netcoreapp2.1\ubuntu.18.04-x64\publish\*
 
-7z a dist\sultan-osx64.zip bin\Release\netcoreapp2.1\osx10.13-x64\publish\*
+7z a dist\sultan-osx64.zip bin\Release\netcoreapp2.1\osx.10.13-x64\publish\*

